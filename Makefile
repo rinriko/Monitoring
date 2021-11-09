3e78f95efc333b435320a7fad78e1cc53be67b7c
rinriko <rinrikosung@hotmail.com>
2021-11-09 12:53:45 -0600

    write csv

--- Makefile
+++ Makefile
@@ -3,10 +3,12 @@
 # Signifies our desired python version
 # Makefile macros (or variables) are defined a little bit differently than traditional bash, keep in mind that in the Makefile there's top-level Makefile-only syntax, and everything else is bash script syntax.
 PYTHON = python3
+INSTALL_PY = python3 -m pip install
+INIT = sudo apt-get install
 
 # .PHONY defines parts of the makefile that are not dependant on any specific file
 # This is most often used to store functions
-.PHONY = help setup run
+.PHONY = help init setup run
 
 # Defines the default target that `make` will to try to make, or in the case of a phony target, execute the specified commands
 # This target is executed whenever we just type `make`
@@ -15,6 +17,7 @@ PYTHON = python3
 # The @ makes sure that the command itself isn't echoed in the terminal
 help:
 	@echo "---------------HELP-----------------"
+	@echo "To first init the project type 'make init'"
 	@echo "To setup the project type 'make setup'"
 	@echo "To run the project type 'make run'"
 	@echo "------------------------------------"
@@ -23,4 +26,7 @@ run:
 	${PYTHON} main.py
 
 setup:
-	python3 -m pip install -r requirements.txt
+	${INSTALL_PY} -r requirements.txt
+
+init:
+	${INIT} powertop dstat
--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import subprocess
 import shlex
+import csv  
+subheader = ['date', 'time', 'usr', 'sys', 'idl', 'wai', 'stl', 'read',  'writ', 'read', 'writ', 'power est.']
+header = ['System', '', 'Total', 'cpu', 'usage', '', '', 'dsk/total',  '', 'io/total', '', 'power est.']
+import csv
 
-def run_command(command):
-    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            print(output.strip().decode("utf-8") )
-    rc = process.poll()
-    return rc
-    
+# open the file in the write mode
+f = open('test.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+# write a row to the csv file
+#writer.writerow([header[0] + header[1]] + [header[2] + ' ' + header[3] + ' ' + header[4] + header[5] + header[6]] + [header[7] + header[8]] + [header[9] + header[10]] + header[11:])
+writer.writerow(header)
+writer.writerow(subheader)
+
+
+
 # system time, total-cpu-usage, dsk/total, io/total
 command = "sudo dstat -tcdr" 
+process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+while True:
+	row = []
+	output = process.stdout.readline()
+	if output == '' and process.poll() is not None:
+		break
+	if output:
+		if "system" not in output.strip().decode("utf-8") and "time" not in output.strip().decode("utf-8"):
+			string = output.strip().decode("utf-8")
+			result = [x.strip() for x in string.split('|')]
+			for text in result:
+				for x in text.split(' '):
+					if x != '':
+						row.append(x.strip())
+			writer.writerow(row)
+rc = process.poll()
+
+
+# close the file
+f.close()
+
+
 # # powertop
 # command = "sudo powertop"
 run_command(command)
